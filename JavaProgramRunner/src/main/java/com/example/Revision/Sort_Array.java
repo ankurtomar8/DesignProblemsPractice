d0de6c2227f9dff68ac90f7a36cda93a5cbbb7cb
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-27 23:35:46 +0530

    feat(docs): Add output comment to `RemoveDuplicates` example
    This commit adds a comment to the `RemoveDuplicates.java` file to show the expected output of the program.
    
    Key changes:
    - **Added Output Comment**:
      - A comment `//[a, b, c, d, e]` has been added to clarify the result of printing the `duplicateSet`.

--- JavaProgramRunner/src/main/java/com/example/Revision/Sort_Array.java
+++ JavaProgramRunner/src/main/java/com/example/Revision/Sort_Array.java
@@ -7,9 +7,9 @@ public class Sort_Array {
         int[] numArray = {3, 1, 4,
         4,5,6,9,7,4,2,3,33,66,787,11,77,445,66,};
 
-        Arrays.stream(numArray).sorted();
+        Arrays.sort(numArray);
         System.out.println(Arrays.toString(numArray));
-        // [3, 1, 4, 4, 5, 6, 9, 7, 4, 2, 3, 33, 66, 787, 11, 77, 445, 66]
+        // [1, 2, 3, 3, 4, 4, 4, 5, 6, 7, 9, 11, 33, 66, 66, 77, 445, 787]
 
     }
 }
