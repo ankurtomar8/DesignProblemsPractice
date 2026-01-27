6070e1c6bbd97371c1328fa73459d37cd183bf6d
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-27 23:22:23 +0530

    feat: Add new Java module and frequency counting example
    This commit introduces a new Java library module and adds an example for calculating character frequency.
    
    Key changes:
    - **New Java Library Module**:
      - Added a new Java library module named `app:lib`.
      - Configured `settings.gradle.kts` to include the new module.
      - Created the `build.gradle.kts` file for the module, setting Java compatibility to version 11.
      - Included a placeholder `MyClass.java` and a `.gitignore` file.
    
    - **Character Frequency Example**:
      - Added a new class `FrequencyOfCharacters.java` in the `JavaProgramRunner` module.
      - This class demonstrates how to count the frequency of characters in an array using a `HashMap`.
    
    - **IDE Configuration**:
      - Added `vcs.xml` to configure Git version control mappings for the project.

---
+++ JavaProgramRunner/src/main/java/com/example/Revision/FrequencyOfCharacters.java
@@ -0,0 +1,24 @@
+package com.example.Revision;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+
+        char[] charArray = {'a','a','a','a','b','b','b','b','c','c','c','c','c'
+        ,'d','d','d','e','e','e'};
+
+        Map<Character,Integer> frequencyMap = new HashMap<>();
+        for(char c : charArray) {
+            if (frequencyMap.containsKey(c)){
+                frequencyMap.put(c,frequencyMap.get(c)+1);
+
+            }else {
+                frequencyMap.put(c,1);
+            }
+        }
+        System.out.println(frequencyMap);
+
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "ProgramRunner"
 include(":app")
 include(":JavaProgramRunner")
+include(":app:lib")
