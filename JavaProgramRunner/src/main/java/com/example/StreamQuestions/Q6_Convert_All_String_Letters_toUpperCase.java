9d4a985ec2cb20f906f021640ca44ee00eba90e7
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-29 23:01:42 +0530

    feat(stream-api): Add example for finding the first non-repeating character
    This commit adds a new Java example demonstrating how to find the first non-repeating character in a string using the Stream API.
    
    The new class, `Q5_find_First_Non_Repeating_Characters`, showcases a solution that:
    - Converts the string to a stream of characters.
    - Filters the characters by checking if their first and last occurrences in the string are the same.
    - Uses `findFirst()` to retrieve the first character that satisfies the condition.

---
+++ JavaProgramRunner/src/main/java/com/example/StreamQuestions/Q6_Convert_All_String_Letters_toUpperCase.java
@@ -0,0 +1,23 @@
+package com.example.StreamQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q6_Convert_All_String_Letters_toUpperCase {
+    public static void main(String[] args) {
+
+        String str  = "Software Engineering Development Hardware";
+
+        List<String> upperCaseList = Arrays.stream(str.split(" "))
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(upperCaseList);
+        // [SOFTWARE, ENGINEERING, DEVELOPMENT, HARDWARE)
+
+
+
+
+    }
+}
