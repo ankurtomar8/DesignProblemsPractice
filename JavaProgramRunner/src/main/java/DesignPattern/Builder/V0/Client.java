6c00506ff256272b83c99b77817ebc617db1c33f
Dev-Ankur <ankurtomar8@outlook.com>
2026-02-05 22:22:55 +0530

    feat(stream): Add string matching example and update existing one
    This commit adds a new Java Stream example for finding matching strings in a list and enhances an existing example.
    
    Key changes:
    - **New String Matching Example**:
      - Added `Q9_Find_If_any_String_matches_in_list.java` to demonstrate how to find strings containing a specific substring (`"Hardware"`) using `stream().filter().collect()`.
    
    - **Updated Uppercase Conversion Example**:
      - In `Q6_Convert_All_String_Letters_toUpperCase.java`, added an intermediate step to explicitly show the result of splitting a string into a `List<String>` before converting it to uppercase.

---
+++ JavaProgramRunner/src/main/java/DesignPattern/Builder/V0/Client.java
@@ -0,0 +1,7 @@
+package DesignPattern.Builder.V0;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JavaProgramRunner/src/main/java/DesignPattern/Builder/V0/SoldItem.java
@@ -0,0 +1,70 @@
+package DesignPattern.Builder.V0;
+
+public class SoldItem {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+    private boolean isPrime;
+    private double discount;
+    private String paymentMethod;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isPrime() {
+        return isPrime;
+    }
+
+    public void setPrime(boolean prime) {
+        isPrime = prime;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+
+}
