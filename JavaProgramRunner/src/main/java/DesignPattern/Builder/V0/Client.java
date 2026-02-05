ad2aef0d68a75b1309457a7d84c2847fd89cade5
Dev-Ankur <ankurtomar8@outlook.com>
2026-02-05 22:25:33 +0530

    feat(stream): Add string matching example and update existing one
    This commit adds a new Java Stream example for finding matching strings in a list and enhances an existing example.
    
    Key changes:
    - **New String Matching Example**:
      - Added `Q9_Find_If_any_String_matches_in_list.java` to demonstrate how to find strings containing a specific substring (`"Hardware"`) using `stream().filter().collect()`.
    
    - **Updated Uppercase Conversion Example**:
      - In `Q6_Convert_All_String_Letters_toUpperCase.java`, added an intermediate step to explicitly show the result of splitting a string into a `List<String>` before converting it to uppercase.

--- JavaProgramRunner/src/main/java/DesignPattern/Builder/V0/Client.java
+++ JavaProgramRunner/src/main/java/DesignPattern/Builder/V0/Client.java
@@ -3,5 +3,17 @@ package DesignPattern.Builder.V0;
 public class Client {
     public static void main(String[] args) {
 
+        SoldItemBuilder soldItemBuilder = new SoldItemBuilder();
+        soldItemBuilder.setId(1);
+        soldItemBuilder.setName("Apple");
+        soldItemBuilder.setQuantity(10);
+        soldItemBuilder.setPrice(100);
+        soldItemBuilder.setPrime(true);
+        soldItemBuilder.setDiscount(10);
+        soldItemBuilder.setPaymentMethod("UPI UPI UPI");
+
+        SoldItem soldItem = new SoldItem();
+        System.out.println(soldItem);
+
     }
 }
