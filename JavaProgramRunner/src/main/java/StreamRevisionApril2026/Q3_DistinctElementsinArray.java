c4ce4af2185579788848728818739964edb9cd73
Dev-Ankur <ankurtomar8@outlook.com>
2026-04-13 17:24:41 +0530

    feat(streams): implement even and odd number filtering using Java Streams
    This commit adds a new demonstration class, `Q1_EvenOdd`, which illustrates how to filter a list of integers into separate even and odd collections using the Java Stream API.
    
    Key changes:
    - **`Q1_EvenOdd.java`**:
      - Initializes a list of integers from 1 to 20.
      - Implements filtering logic using `.stream().filter()` with predicate logic (`num % 2 == 0` for even and `num % 2 != 0` for odd).
      - Utilizes `Collectors.toList()` to aggregate the filtered results.
      - Includes console output to verify the contents of the resulting `evenList` and `oddList`.

---
+++ JavaProgramRunner/src/main/java/StreamRevisionApril2026/Q3_DistinctElementsinArray.java
@@ -0,0 +1,22 @@
+package StreamRevisionApril2026;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q3_DistinctElementsinArray {
+    public static void main(String[] args) {
+
+        List<Integer> list =
+                Arrays.asList(1,2,4,6,7,8,6,5,4,7,1,71,7,2,7,13,4,2,1,2,3,34,5,4,6,6);
+
+        List<Integer> distinctList = list.stream()
+                .distinct() // finds the distinct elements
+                .collect(Collectors.toList());
+
+        System.out.println(distinctList);
+        //Output [1, 2, 4, 6, 7, 8, 5, 71, 13, 3, 34]
+
+
+    }
+}
