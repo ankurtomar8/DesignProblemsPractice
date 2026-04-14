8ce7c746dbca7f2ba9c2259d521c97acafb279a8
Dev-Ankur <ankurtomar8@outlook.com>
2026-04-14 16:27:11 +0530

    feat(stream): implement sum calculation for list elements
    This commit adds a new utility class, `Q7_Sum_of_List_Elements`, which demonstrates how to calculate the total sum of an integer list using the Java Stream API.
    
    Key changes:
    - **`Q7_Sum_of_List_Elements.java`**:
      - Initializes a list of integers from 1 to 10.
      - Utilizes `stream()`, `mapToInt()`, and `sum()` to efficiently aggregate the list elements.
      - Outputs the final sum to the console.

---
+++ JavaProgramRunner/src/main/java/StreamRevisionApril2026/Q_9_Find_if_any_String_Matches_in_list.java
@@ -0,0 +1,25 @@
+package StreamRevisionApril2026;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q_9_Find_if_any_String_Matches_in_list {
+    public static void main(String[] args) {
+
+        List<String> listStr = Arrays.asList
+                ("API","LearningNewAPI","NeedtodevepNewAPI","whatiswhat");
+
+        List<String> match = listStr.stream()
+                .filter(str->str.contains("API"))
+                .collect(Collectors.toList());
+        System.out.println(match);
+        //Output [API, LearningNewAPI, NeedtodevepNewAPI]
+
+        String convert = String.join(",",match);
+        System.out.println(convert);
+        //Output API,LearningNewAPI,NeedtodevepNewAPI
+
+
+    }
+}
