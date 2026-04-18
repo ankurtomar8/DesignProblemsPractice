a15b70ff43372b02ae8b19ba1f14092758a01ed2
Dev-Ankur <ankurtomar8@outlook.com>
2026-04-18 17:00:27 +0530

    feat(string): implement sentence reversal using a loop
    This commit introduces a new utility class, `ReverseSentenceUsingLoop`, which demonstrates how to reverse the order of words in a given string using basic array manipulation and a for-loop.
    
    Key changes:
    - **`ReverseSentenceUsingLoop.java`**:
      - Implements logic to split a sentence into words based on space delimiters.
      - Utilizes a reverse `for` loop to iterate through the word array and reconstruct the sentence in reverse order.
      - Includes a `main` method with a sample input and console output to verify the reversal logic.

--- JavaProgramRunner/src/main/java/com/example/RevisionV1/FrequencyOfCharacters.java
+++ JavaProgramRunner/src/main/java/com/example/RevisionV1/FrequencyOfCharacters.java
@@ -6,19 +6,20 @@ import java.util.Map;
 public class FrequencyOfCharacters {
     public static void main(String[] args) {
 
-        char[] arr = {'a','a','a','a','b','b','b','b','c','c','c','c','c','d','d','d'};
+        char[] charArray = {'a','a','a','a','b','b','b','b','c','c','c','c','c'
+                ,'d','d','d','e','e','e'};
 
         Map<Character,Integer> frequencyMap = new HashMap<>();
 
-        for(char c : arr) {
-            if(frequencyMap.containsKey(c)) {
+        for(char c : charArray){
+            if(frequencyMap.containsKey(c)){
                 frequencyMap.put(c,frequencyMap.get(c)+1);
-        }else{
-            frequencyMap.put(c,1);
-        }
+            }else {
+                frequencyMap.put(c,1);
+            }
         }
         System.out.println(frequencyMap);
-        //Output: {a=4, b=4, c=5, d=3}
+        //Output   {a=4, b=4, c=5, d=3, e=3}
 
 
     }
