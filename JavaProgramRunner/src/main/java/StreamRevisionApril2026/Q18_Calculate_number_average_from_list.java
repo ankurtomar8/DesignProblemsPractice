e0adfd0f66b23ce144caf246d5545120e873cc05
Dev-Ankur <ankurtomar8@outlook.com>
2026-04-21 21:55:16 +0530

    feat(stream): implement null object removal from a list
    This commit introduces a new utility class, `Q17_Removing_null_objects`, which demonstrates how to filter out null values from a list of strings using the Java Stream API.
    
    Key changes:
    - **`Q17_Removing_null_objects.java`**:
      - Initializes a list of strings containing several `null` elements.
      - Utilizes `stream().filter(Objects::nonNull)` to exclude null references from the stream.
      - Collects the filtered results into a new list and prints it to the console to verify the output.

---
+++ JavaProgramRunner/src/main/java/StreamRevisionApril2026/Q18_Calculate_number_average_from_list.java
@@ -0,0 +1,21 @@
+package StreamRevisionApril2026;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q18_Calculate_number_average_from_list {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        double average = numbers
+                .stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+
+        System.out.println(average);
+        // Output : 5.0
+
+    }
+}
