80644baae62abd656a08cd9d122f2740fbe88bc9
Dev-Ankur <ankurtomar8@outlook.com>
2026-08-02 11:51:12 +0530

    print a array

---
+++ JavaProgramRunner/src/main/java/IntermmediateOneRefresher/Arrays/PrintAArray.java
@@ -0,0 +1,13 @@
+package IntermmediateOneRefresher.Arrays;
+
+public class PrintAArray {
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 1, 7, 5, 9, 6, 3, 74};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" "+array[i]);
+        }
+        System.out.println();
+    }
+}
