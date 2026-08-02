7210493a687f5c357f33f7c9a6093739d0e529cf
Dev-Ankur <ankurtomar8@outlook.com>
2026-08-02 11:54:47 +0530

    rotate a array K times

---
+++ JavaProgramRunner/src/main/java/IntermmediateOneRefresher/Arrays/RotateKTimesArray.java
@@ -0,0 +1,35 @@
+package IntermmediateOneRefresher.Arrays;
+
+public class RotateKTimesArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        if (k == 0) return;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        RotateKTimesArray solution = new RotateKTimesArray();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        solution.rotate(nums, k);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        // Output 5 6 7 1 2 3 4
+    }
+}
