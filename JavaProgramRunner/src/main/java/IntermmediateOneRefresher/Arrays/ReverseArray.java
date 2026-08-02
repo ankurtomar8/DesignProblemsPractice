550fbf30eceb7f98e3e8e34d61599b1cb353f426
Dev-Ankur <ankurtomar8@outlook.com>
2026-08-02 11:52:14 +0530

    reverse a array

---
+++ JavaProgramRunner/src/main/java/IntermmediateOneRefresher/Arrays/ReverseArray.java
@@ -0,0 +1,24 @@
+package IntermmediateOneRefresher.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        reverse(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void reverse(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
